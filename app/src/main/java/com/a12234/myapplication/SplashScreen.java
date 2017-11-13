1df1f69d12242cbf04403a3e9c496220d6bdc018
Merinaphing <33473496+Merinaphing@users.noreply.github.com>
2017-11-13 21:43:48 +0700

    Initial commit

---
+++ app/src/main/java/com/a12234/myapplication/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.a12234.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Meirina on 13/11/2017.
+ */
+
+public class SplashScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+
+    }
+}
